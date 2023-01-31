5163a57d95e53f592ede1a2aea89f00342bbb846
klaudiakopec <92815040+klaudiakopec@users.noreply.github.com>
2023-01-31 07:41:51 +0100

    Add files via upload

---
+++ kolo2a/src/com/company/LinkedListIterator.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class LinkedListIterator {
+    private ListElem currentElem;
+
+    public LinkedListIterator(LinkedList linkedList)
+    {
+        currentElem = linkedList.getFirst();
+    }
+
+    public boolean hasNext()
+    {
+        if (currentElem==null) return false;
+        return currentElem!=null;
+    }
+
+    public ListElem next()
+    {
+        if (currentElem==null) return null;
+        ListElem currElem = currentElem;
+        currentElem = currentElem.next;
+        return currElem;
+    }
+}
